059480dc03041bd201906bb72114c4895063f8d6
Leo espinoza <leo.spinoza@yahoo.com>
2020-10-07 10:05:18 -0500

    :sunglasses: initial commit :star:

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,79 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import country from '../xx1.json'
+import sa from '../xx.json'
+
 
 export default function Home() {
+
+  // do not forget country code
+  // country code lookup npm
+  // also flags npm
+
+// search by country
+// axios('https://fr1.api.radio-browser.info/json/stations/bycountry/mexico')
+
+// search by genre
+// axios('https://fr1.api.radio-browser.info/json/stations/bytagexact/70s')
+
+
+axios('https://fr1.api.radio-browser.info/json/stations/bytagexact/classical')
+
+// axios('https://fr1.api.radio-browser.info/json/tags')
+// axios('https://fr1.api.radio-browser.info/json/countries')
+  .then(res => {
+    // let data = JSON.stringify(res.data)
+    let data = res.data
+    console.log(data)
+    let europe = sortList(country.europe)
+    let asia = sortList(country.asia)
+    let africa = sortList(country.africa)
+    let australia = sortList(country.australia)
+    let nAm = sortList(country.nAmerica)
+    let sAm = sortList(country.sAmerica)
+
+
+
+    // let europe = country[2].asia
+    
+    function sortList(continent){
+      return continent.map(name => {
+        for(let x = 0; x < data.length; x++){
+          if(data[x].name === name) return data[x]
+        }
+      })
+    }
+    
+
+
+    // for (const key in country){
+    //   country[key]
+    // }
+
+    // const saa = Object.keys(country)
+    // console.log(saa)
+
+
+
+    let samp = data.sort((a, b)=>{
+      var textA = a.votes;
+      var textB = b.votes;
+      return (textA > textB) ? -1 : (textA < textB) ? 1 : 0;
+    })
+
+    // console.log(samp)
+    // console.log(JSON.stringify(samp))
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
+
+// console.log(sa)
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,54 +84,28 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {/* <a href="http://direct.francebleu.fr/live/fbgascogne-midfi.mp3"
+            target='_blank'> */}
+            
+          <a href='http://19183.live.streamtheworld.com:80/OLI968FMAAC.aac'
+            target='_blank'>
+            <button>La Clinique Radiophonique</button>
+          </a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        video
+        <audio controls>
+          <source src="http://20283.live.streamtheworld.com:3690/XHRED_FMAAC_SC" type="audio/mpeg" />
+          <source src="http://20283.live.streamtheworld.com:3690/XHRED_FMAAC_SC" type="audio/ogg" />
+          <p>Your browser does not support HTML5 audio, but you can still 
+            <a href="audiofile.mp3">download the music</a>.</p>
+        </audio>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+       
     </div>
   )
 }
+
+
+
+
+
---
+++ src/utils/apikey.js
@@ -0,0 +1,5 @@
+
+const app_name = 'Radio Listen'
+const api_key = '6b0a6a425c37d863465d8c5cbad8e6a9'
+const shared_secret = '72a832161b8cd307a72c7fd9ec378a04'
+const registered_to = 'luisFern12'
